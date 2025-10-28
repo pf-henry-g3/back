2404b34c6f2050ed6482fef74f6c33c0ed22e810
agosgaggioli <gaggioliagostina@gmail.com>
2025-10-28 15:42:15 -0300

    feat(vacancy) : se implemento la entidad

--- src/module/vacancy/entities/vacancy.entity.ts
+++ src/module/vacancy/entities/vacancy.entity.ts
@@ -1,1 +1,48 @@
-export class Vacancy {}
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: "Vacancies" })
+export class Vacancy {
+    
+    @PrimaryGeneratedColumn("uuid")
+    id: string
+
+    @Column({
+        type: "varchar",
+        length: 50,
+        nullable: false,
+    })
+    name: string
+
+    @Column({
+        type: "text",
+        length: 50,
+        nullable: false,
+    })
+    vacancyDescription: string
+
+    @Column({
+        default: "TRUE"
+    })
+    isOpen: boolean
+
+    @Column({
+        default: "NO IMAGE"
+    })
+    vacancyImage: string
+
+    @Column({
+        type: "text",
+        length: 50,
+        nullable: false,
+    })
+    owerType: string
+
+    // muchas vacantes {pertenecen} un usuario 
+    @ManyToOne(() => User, (u) => u.vacantes, {
+        nullable: false,          // pertenece SIEMPRE a un usuario
+        onDelete: 'CASCADE',      // se borra al borrar el usuer 
+        eager: false,
+    })
+    @JoinColumn({ name: 'owerId' })
+    owerId: User;
+}
