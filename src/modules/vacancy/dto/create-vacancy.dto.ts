08c72840741c1b679320765706eb9e8c5b755a42
agosgaggioli <gaggioliagostina@gmail.com>
2025-10-29 16:09:12 -0300

    coreccion ramas agostina entity vacancy

--- src/modules/vacancy/dto/create-vacancy.dto.ts
+++ src/modules/vacancy/dto/create-vacancy.dto.ts
@@ -1,5 +1,35 @@
-
-
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString, MaxLength } from "class-validator";
 export class CreateVacancyDto {
+    @ApiProperty({
+        example: "Busqueda Guitarrista"
+    })
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(50)
+    name: string;
 
+    @ApiProperty({
+        example: "Se busca guitarrista con experiencia en pop +5 años"
+    })
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(50)
+    vacancyDescription: string
+
+
+    @ApiProperty({
+        example: "Evento"
+    })
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(50)
+    owerType: string
+
+    @ApiProperty({
+        example: "UUID"
+    })
+    @IsString()
+    @IsNotEmpty()
+    owerId: string;
 }
