4e20b9da1856f167b27621d8ccad17e905a00023
Santiago <santiagoriv06@gmail.com>
2025-10-29 11:35:05 -0300

    feat(band) implement POST create() controller and service

--- src/modules/band/band.controller.ts
+++ src/modules/band/band.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { BandService } from './band.service';
+import { CreateBandDto } from './dto/create-band.dto';
 
 @Controller('band')
-export class BandController { }
+export class BandController {
+    constructor(private readonly bandService: BandService) { }
+
+    @Post()
+      create(@Body() createBandDto: CreateBandDto) {
+        return this.bandService.create(createBandDto);
+        // Aca tengo que definir que datos y como me van a llegar para crear una banda
+      }    
+ }
--- src/modules/band/band.service.ts
+++ src/modules/band/band.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Not, Repository } from 'typeorm';
+import { Band } from './entities/band.entity';
+import { CreateBandDto } from './dto/create-band.dto';
 
 @Injectable()
-export class BandService { }
+export class BandService { 
+    constructor(
+        @InjectRepository(Band)
+        private readonly bandRepository: Repository<Band>,
+    ) { }
+
+    create(createBandDto: CreateBandDto) {
+        const band = this.bandRepository.create(createBandDto);
+        return this.bandRepository.save(band);
+    }
+}
