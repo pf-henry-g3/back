b267a8b9e334f02771bc8b975fbc893972038d43
Carlos <carlosmogo94@gmail.com>
2025-11-06 17:35:59 -0300

    verificacion por email implementada

---
+++ src/modules/user/dto/user-response.dto.ts
@@ -0,0 +1,67 @@
+import { Exclude, Expose, Transform, Type } from 'class-transformer';
+import { GenreResponseDto } from 'src/common/dto/genre-response.dto';
+import { RoleResponseDto } from 'src/common/dto/role-response.dto';
+
+export class UserResponseDto {
+    @Expose()
+    id: string;
+
+    @Expose()
+    userName: string;
+
+    @Expose()
+    email: string;
+
+    @Expose()
+    name: string;
+
+    @Expose()
+    aboutMe: string;
+
+    @Expose()
+    urlImage: string;
+
+    @Expose()
+    city: string;
+
+    @Expose()
+    country: string;
+
+    @Expose()
+    averageRating: number;
+
+    @Expose()
+    isVerified: boolean;
+
+
+    @Exclude()
+    password: string;
+
+    @Exclude()
+    address: string;
+
+    @Exclude()
+    authProviderId: string;
+
+    @Exclude()
+    latitude: number;
+
+    @Exclude()
+    longitude: number;
+
+    @Exclude()
+    deleteAt: Date;
+
+    @Expose()
+    @Type(() => GenreResponseDto)
+    genres: GenreResponseDto[];
+
+    @Expose()
+    @Type(() => RoleResponseDto)
+    @Transform(({ value }) =>
+        Array.isArray(value)
+            ? value.filter(role => !['Admin', 'SuperAdmin'].includes(role.name))
+            : []
+    )
+    roles: RoleResponseDto[];
+}
