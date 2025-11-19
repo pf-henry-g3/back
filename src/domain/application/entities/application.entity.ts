460fa805fffe6af908436564f4ccc6130d8a97c2
agosgaggioli <gaggioliagostina@gmail.com>
2025-11-19 08:04:32 -0300

    feat(application): agregue la entidad y su  dto

---
+++ src/domain/application/entities/application.entity.ts
@@ -0,0 +1,32 @@
+import { User } from "src/domain/user/entities/user.entity";
+import { Vacancy } from "src/domain/vacancy/entities/vacancy.entity";
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Application {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+  @ManyToOne(() => Vacancy, (vacancy) => vacancy.applications, {
+    nullable: false,
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'vacancyId' })
+  vacancyId: Vacancy;
+
+  @ManyToOne(() => User, (user) => user.applicationsAsApplicant, {
+    nullable: false,
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'applicantId' })
+  applicantId: User;
+
+  @Column({ type: 'timestamptz' })
+  applicationDate: Date;
+
+  @Column({ type: 'text', nullable: true })
+  applicationDescription: string;
+
+  @Column({ length: 50, default: 'PENDING' })
+  status: string;
+}
