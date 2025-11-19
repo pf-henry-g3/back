fe54902a05c3222f01473ac4ea1cd3b8f286ad57
agosgaggioli <gaggioliagostina@gmail.com>
2025-11-19 11:02:59 -0300

    guardado temporal

--- src/domain/application/application.module.ts
+++ src/domain/application/application.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ApplicationService } from './application.service';
 import { ApplicationController } from './application.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../user/entities/user.entity';
+import { Vacancy } from '../vacancy/entities/vacancy.entity';
 
 @Module({
+    imports: [
+
+      TypeOrmModule.forFeature([User, Vacancy]),
+
+    ],
   controllers: [ApplicationController],
   providers: [ApplicationService],
 })
--- src/domain/application/application.service.ts
+++ src/domain/application/application.service.ts
@@ -1,11 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateApplicationDto } from './dto/create-application.dto';
 import { UpdateApplicationDto } from './dto/update-application.dto';
+import { User } from '../user/entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Vacancy } from '../vacancy/entities/vacancy.entity';
+import { Application } from './entities/application.entity';
 
 @Injectable()
 export class ApplicationService {
-  create(createApplicationDto: CreateApplicationDto) {
-    return 'This action adds a new application';
+  constructor(
+
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+
+    @InjectRepository(Vacancy)
+    private readonly vacancyRepository: Repository<Vacancy>,
+     @InjectRepository(Application)
+    private readonly apliRepository: Repository<Application>,
+
+  ) { }
+
+  async create(createApplicationDto: CreateApplicationDto) {
+  const { vacancyId, applicantId, applicationDescription } = createApplicationDto;
+
+    
+    const vacancy = await this.vacancyRepository.findOne({
+      where: { id: vacancyId },
+    });
+
+    if (!vacancy) {
+      throw new BadRequestException('Vacante no encontrada');
+    }
+
+    const applicant = await this.usersRepository.findOne({
+      where: { id: applicantId },
+    });
+
+    if (!applicant) {
+      throw new BadRequestException('postulacion no encontrada');
+    }
+
+
+    const existingApplication = await this.apliRepository.findOne({
+      where: {
+        vacancyId: { id: vacancyId },
+        applicantId: { id: applicantId },
+      },
+    });
+
+    if (existingApplication) {
+      throw new BadRequestException(
+        'el usuario ya aplico',
+      );
+    }
+    const application = this.apliRepository.create({
+      vacancyId: vacancy,
+      applicantId: applicant,
+      applicationDescription,
+      applicationDate: new Date(),  
+    });
+
+    return await this.apliRepository.save(application);
   }
 
   findAll() {
