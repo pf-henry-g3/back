09df709551f45a49a92b1185210af09432aaec61
pf-henry-g3 <pf.henry.g3@gmail.com>
2025-11-20 15:17:05 -0300

    Update mailer.config.ts

--- src/core/mailer/mailer.config.ts
+++ src/core/mailer/mailer.config.ts
@@ -5,7 +5,8 @@ import { join } from 'path';
 export const mailerConfig = (): MailerOptions => ({
     transport: {
         host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
+        port: 465, 
+        secure: true,
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS,
