f7cfbf1bc8de26a8c384582b1ff61716735a045b
Carlos <carlosmogo94@gmail.com>
2025-11-20 11:26:15 -0300

    feat(Application): Implementar borrado y busqueda individual doc(Application): Documentar rutas en Swagger test(Application): Probar endpoints en swagger

--- src/common/factories/OwnerOrAdmin.factory.ts
+++ src/common/factories/OwnerOrAdmin.factory.ts
@@ -2,7 +2,9 @@ import { Band } from 'src/domain/band/entities/band.entity';
 import { Vacancy } from 'src/domain/vacancy/entities/vacancy.entity';
 import { OwnerOrAdminGuard } from '../guards/OwnerOrAdmin.guard';
 import { Review } from 'src/domain/review/entities/review.entity';
+import { Application } from 'src/domain/application/entities/application.entity';
 
 export const BandOwnerGuard = () => OwnerOrAdminGuard(Band, 'leader');
 export const VacancyOwnerGuard = () => OwnerOrAdminGuard(Vacancy, 'owner');
 export const ReviewOwnerGuard = () => OwnerOrAdminGuard(Review, 'owner');
+export const ApplicationOwnerGuard = () => OwnerOrAdminGuard(Application, 'applicantId');
--- src/domain/application/application.service.ts
+++ src/domain/application/application.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateApplicationDto } from './dto/create-application.dto';
 import { UpdateApplicationDto } from './dto/update-application.dto';
 import { User } from '../user/entities/user.entity';
@@ -6,6 +6,9 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Vacancy } from '../vacancy/entities/vacancy.entity';
 import { Application } from './entities/application.entity';
+import { plainToInstance } from 'class-transformer';
+import { ApplicationResponseDto } from './dto/application-response.dto';
+import { Pages } from 'src/common/enums/pages.enum';
 
 @Injectable()
 export class ApplicationService {
@@ -16,15 +19,15 @@ export class ApplicationService {
 
     @InjectRepository(Vacancy)
     private readonly vacancyRepository: Repository<Vacancy>,
-     @InjectRepository(Application)
+    @InjectRepository(Application)
     private readonly apliRepository: Repository<Application>,
 
   ) { }
 
-  async create(createApplicationDto: CreateApplicationDto) {
-  const { vacancyId, applicantId, applicationDescription } = createApplicationDto;
+  async create(applicantId: string, createApplicationDto: CreateApplicationDto) {
+    const { vacancyId, applicationDescription } = createApplicationDto;
 
-    
+
     const vacancy = await this.vacancyRepository.findOne({
       where: { id: vacancyId },
     });
@@ -38,7 +41,7 @@ export class ApplicationService {
     });
 
     if (!applicant) {
-      throw new BadRequestException('postulacion no encontrada');
+      throw new BadRequestException('Usuario no encontrado');
     }
 
 
@@ -47,54 +50,87 @@ export class ApplicationService {
         vacancyId: { id: vacancyId },
         applicantId: { id: applicantId },
       },
+      select: ['id']
     });
 
-    if (existingApplication) {
-      throw new BadRequestException(
-        'el usuario ya aplico',
-      );
-    }
+    if (existingApplication) throw new BadRequestException('el usuario ya aplico a esta vacante');
+
     const application = this.apliRepository.create({
       vacancyId: vacancy,
       applicantId: applicant,
       applicationDescription,
-      applicationDate: new Date(),  
+      applicationDate: new Date(),
     });
 
-    return await this.apliRepository.save(application);
+    await this.apliRepository.save(application);
+
+    return plainToInstance(ApplicationResponseDto, application, {
+      excludeExtraneousValues: true,
+    })
   }
 
-  findAll() {
-    return `This action returns all application`;
+  async findAll(page: number = Pages.Pages, limit: number = Pages.Limit) {
+    const [applications, total] = await this.apliRepository.findAndCount({
+      skip: (page - 1) * limit,
+      take: limit,
+      relations: {
+        vacancyId: true,
+        applicantId: true,
+      }
+    });
+
+    if (!applications) throw new NotFoundException("Postualciones no encontrados");
+
+    const transformedApplications = plainToInstance(ApplicationResponseDto, applications, {
+      excludeExtraneousValues: true,
+    });
+
+    const meta = { total, page, limit };
+
+    return { transformedApplications, meta };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} application`;
+  async findOne(id: string) {
+    let foundApplication: Application | null = await this.apliRepository.findOne({
+      where: { id: id },
+      relations: {
+        vacancyId: true,
+        applicantId: true,
+      }
+    });
+
+    if (!foundApplication) throw new BadRequestException('Postulacion no encontrada')
+
+    const transformedApplication = plainToInstance(ApplicationResponseDto, foundApplication, {
+      excludeExtraneousValues: true,
+    })
+
+    return transformedApplication;
   }
 
-  update(id: number, updateApplicationDto: UpdateApplicationDto) {
-    return `This action updates a #${id} application`;
-  }
+  async softDelete(id: string) {
+    const foundApplication: Application | null = await this.apliRepository.findOneBy({ id });
 
-  remove(id: number) {
-    return `This action removes a #${id} application`;
+    if (!foundApplication) throw new NotFoundException('Vacante no encontrado');
+
+    return await this.apliRepository.softDelete(id)
   }
 
   async findByUser(userId: string) {
-  const applications = await this.apliRepository.find({
-    where: {
-      applicantId: {
-        id: userId,
+    const applications = await this.apliRepository.find({
+      where: {
+        applicantId: {
+          id: userId,
+        },
       },
-    },
-    relations: {
-      vacancyId: true,
-    },
-    order: {
-      applicationDate: 'DESC',
-    },
-  });
+      relations: {
+        vacancyId: true,
+      },
+      order: {
+        applicationDate: 'DESC',
+      },
+    });
 
-  return applications;
-}
+    return applications;
+  }
 }
